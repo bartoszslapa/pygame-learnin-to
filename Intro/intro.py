5e22f29c45d0f730767bcb1c38643cffee4fe496
Bartosz Słapa <useyour@email.com>
2021-05-29 02:39:03 +0200

    Main loop and quit-handling.

--- Intro/intro.py
+++ Intro/intro.py
@@ -1,1 +1,13 @@
 import pygame as pg
+
+pg.init()
+win = pg.display.set_mode((500, 500))
+pg.display.set_caption("Hello Gro.")
+
+running = True
+
+while(running):
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+            print("Quitting.")
