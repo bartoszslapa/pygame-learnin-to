4d600d3e0121843527af19d555eb647a5dc283e9
Bartosz Słapa <useyour@email.com>
2021-05-29 02:48:24 +0200

    Cosmetics and comments.

--- Intro/intro.py
+++ Intro/intro.py
@@ -7,19 +7,23 @@ RECT = (10, 10, 50, 50)
 GREEN = (0, 255, 0)
 
 
-
+# init pygame modules
 pg.init()
+
+# init a window
 WIN = pg.display.set_mode((500, 500))
 pg.display.set_caption("Hello Game.")
 
+# main loop
 running = True
-
-while(running):
+while running:
+    #event handling
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
             print("Quitting.")
     
+    # draw a green rectangle to window WIN and update the window
     pg.draw.rect(WIN, GREEN, RECT)
     pg.display.update()
     
